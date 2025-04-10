003a12f019beab7a6c660f225b0afc4b2c4189d6
LorienFB <lorienfbl@gmail.com>
2025-04-10 17:47:31 +0200

    Projeto inicial

---
+++ src/conectaDAO.java
@@ -0,0 +1,36 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/leiloesUC11",
+                     "root",
+                    "Mysql_1234"
+            );
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
