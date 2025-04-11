a3aa34ef93e5d59419f4bac219764c434fd1b929
LorienFB <lorienfbl@gmail.com>
2025-04-11 18:49:18 +0200

    Ajuste no conectaDAO

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,23 +4,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
 public class conectaDAO {
-    
-    public Connection connectDB(){
+
+    public Connection connectDB() {
         Connection conn = null;
-        
-        try {
-        
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/leiloesUC11",
-                     "root",
-                    "Mysql_1234"
-            );
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+
+      try {
+            String url = "jdbc:mysql://localhost:3306/leiloesUC11";
+            String user = "root";
+            String password = "Mysql_1234";
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão com banco de dados realizada com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Erro de conexão: " + e.getMessage());
         }
+
         return conn;
     }
-    
 }
